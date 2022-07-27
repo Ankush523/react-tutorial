d63f815c9523977aa1560bdb9c781e6233313d23
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-28 00:36:18 +0530

    center img

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,8 @@
 import React from 'react'
 const Book = () => {
   return (
-    <div className=' bg-white rounded-md'>
-        <img className='flex flex-row justify-center w-[200px] h-[200px]' src="https://images-na.ssl-images-amazon.com/images/I/41OiDvq9pDS._SX460_BO1,204,203,200_.jpg" alt=""/>
+    <div className='flex flex-col items-center  bg-white rounded-md'>
+        <img className='w-[200px] h-[200px]' src="https://images-na.ssl-images-amazon.com/images/I/41OiDvq9pDS._SX460_BO1,204,203,200_.jpg" alt=""/>
         <h1>The Complete Novels of Sherlock Holmes</h1>
         <h3>Arthur Conan Doyle</h3>
     </div>
