220224bf0ed9c3c335522c78a5c21c47b3919911
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-29 01:11:37 +0530

    mapping

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import './App.css';
 import BookData from './components/BookData';
 
 function App() {
-
-  const firstBook = {
+  const books = [
+  {
+    id:1,
     img : "https://images-na.ssl-images-amazon.com/images/I/41OiDvq9pDS._SX460_BO1,204,203,200_.jpg",
     title : 'The Complete Novels of Sherlock Holmes',
     author : 'Sir Arthur Conan Doyle'
-  };
+  },
   
-  const secondBook = {
+  {
+    id:2,
     img : "https://images-eu.ssl-images-amazon.com/images/I/4180omRGrSL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg",
     title : 'You Can',
     author : 'George Matthew Adams '
-  };
+  },
+];
   return (
     <div className="App">
-      <BookData value={firstBook} />
-      <BookData value={secondBook} />
+      <section>
+        {books.map((book) => {
+          return(
+          <BookData key={book.id} value={book} />
+          )
+        })}
+      </section>
+
     </div>
   );
 }
