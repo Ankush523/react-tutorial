7f498e303a4ef17b5cd1a144d08edc22e7607c59
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-29 20:28:57 +0530

    events

--- src/components/BookData.js
+++ src/components/BookData.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const BookData = ({value}) => {
+    const buttonHandler = () => {
+        alert('Hello World');
+    }
     return ( 
         <div className='flex flex-col items-center bg-white rounded-xl m-8 p-5'>
             <img className='w-[200px] h-[200px]' src={value.img} alt=""/>
             <h1>{value.title}</h1>
             <h3>{value.author}</h3>
+            <button onClick={buttonHandler}>Click</button>
         </div>
      );
 }
